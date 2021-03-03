e8553d63377a09774e8f8405c1887f5c30c0654d
gustavocabralsouza <gustavocsouza65@gmail.com>
2021-03-02 23:00:17 -0300

    tests files profile

--- src/components/HeaderAplication.tsx
+++ src/components/HeaderAplication.tsx
@@ -3,6 +3,7 @@ export function HeaderAplication(){
         <header>
             <div className="container">
                 <h1>topo da pagina</h1>
+                <button>teste personalizado da minha aplicação</button>
             </div>
         </header>
     );
