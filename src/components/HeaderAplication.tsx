e4a7c92924c862448e3d97e57ec2bcdc76db2133
gustavocabralsouza <gustavocsouza65@gmail.com>
2021-03-02 23:10:28 -0300

    inserção de novos deficientes para testes efetivos

--- src/components/HeaderAplication.tsx
+++ src/components/HeaderAplication.tsx
@@ -1,9 +1,11 @@
+import styles from '../styles/components/HeaderAplication.module.css'
+
 export function HeaderAplication(){
     return (
         <header>
             <div className="container">
                 <h1>topo da pagina</h1>
-                <button>teste personalizado da minha aplicação</button>
+                <button>insira seu JoinThemder !!</button>
             </div>
         </header>
     );
