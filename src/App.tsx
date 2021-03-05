3ad04bd7722ed30b8e89ee65ce29fa17cb1bf8f5
gustavocabralsouza <gustavocsouza65@gmail.com>
2021-03-05 15:39:30 -0300

    project routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { HeaderAplication } from './components/HeaderAplication';
 import './styles/global.css';
 
 
 function App() { //No react nunca pode ter varios componentes debaixo do outro sem uma div por volta deles
   return (
     <div className="container">
-      <HeaderAplication />
+      <div className="navbar">
+        <div className="logo"></div>
+        <nav>
+          <ul>
+            <li><a href="">Home</a></li>
+          </ul>
+        </nav>
+        <img className="cart" />
+      </div>
     </div>
-  );
-}
+  );}
 
 
 export default App;
